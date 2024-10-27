d2fbfc3da384ec3db4f779b063a01ed21261a092
manuel flückiger <manuel@MWorkstation.>
2024-10-27 02:06:33 +0200

    last update for the day

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,5 @@
 return {
-          'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  {'nvim-telescope/telescope.nvim', tag = '0.1.8',
             -- or                              , branch = '0.1.x',
           dependencies = { 'nvim-lua/plenary.nvim' },
           config = function()
@@ -7,5 +7,21 @@ return {
             vim.keymap.set('n', '<C-P>', builtin.find_files, {})
             vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
           end
-        }
+        },{
+        'nvim-telescope/telescope-ui-select.nvim',
+        config = function()
+          -- This is your opts table
+              require("telescope").setup {
+              extensions = {
+                ["ui-select"] = {
+                  require("telescope.themes").get_dropdown {
+                    -- even more opts
+                  }
+                }
+              }
+            }
+            require("telescope").load_extension("ui-select")
+        end
+        },
+      }
 
